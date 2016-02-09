3e097f2e6b3b6b7706708bd4d690cf7dca3a3b3c
Zhantao Peng <peng.z@husky.neu.edu>
2016-02-09 18:59:39 -0500

    draft

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
 function setup() {
-  
+  var ellipseX = random(0,600);
+  var ellipseY = random(0,600);
+  createCanvas(600,600)
+  background(255)
+    ellipse(ellipseX,ellipseY, 10,10)
+    text("mine",ellipseX,ellipseY)
 }
 
 function draw() {
-  
-}
+  if(mouseIsPressed ){
+    noFill()
+    ellipse(mouseX,mouseY,100,100)
+  }else{
+    
+  }
+    
+  }
+
+
+//how to calculate the distance between the mouse and the mine?
+//when click the mouse, a circle come out which indicate the distance between the mine and mouse by its size. That circle 
+//should disapper after mouse release, not dispaly on screen forever.
